5ad61c13f6cd4ded497e9a06e4d56a6d9e002459
yifeng2019uwb <yifeng2019@gmail.com>
2026-06-12 13:08:56 -0700

    add sample of service code

---
+++ front-end/src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '20vh', fontFamily: 'sans-serif' }}>
+      <h1>Welcome to React</h1>
+      <p>This is a test service page deployed on DigitalOcean App Platform.</p>
+    </div>
+  )
+}
