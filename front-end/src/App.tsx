7580fed0a9544aea1d7ff06b0b0b2eeffbf53780
yifeng2019uwb <yifeng2019@gmail.com>
2026-06-12 13:28:52 -0700

    add the do deploy template

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -3,6 +3,7 @@ export default function App() {
     <div style={{ textAlign: 'center', marginTop: '20vh', fontFamily: 'sans-serif' }}>
       <h1>Welcome to React</h1>
       <p>This is a test service page deployed on DigitalOcean App Platform.</p>
+      <p>Build from github</p>
     </div>
   )
 }
